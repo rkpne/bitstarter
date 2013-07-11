44a3db87bc21e290190d0e42a6f012530372cd7d
Ramakrishna Prasad <rkpne@yahoo.com>
2013-07-11 03:16:15 +0000

    Second attempt

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var content = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-var fs = require('fs');
-var buffer = new buffer ();
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
- });
+  response.send(content.toString("utf-8"));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
