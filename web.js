eca5ad9639cbe06ce598f61fc1d084e550d49eaa
Ramakrishna Prasad <rkpne@yahoo.com>
2013-07-11 02:46:45 +0000

    replaced file read

--- web.js
+++ web.js
@@ -1,10 +1,6 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+var fs = require("fs");
+var data = fs.readFileSync("index.html", "utf8");
+console.log(data);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
